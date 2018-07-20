2e4c84061a0f09a885ca03f13715115a32b4152b
Yomaira Escano <yjescano@gmail.com>
2018-07-20 04:13:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 // Write your code in this file!
 function scuberGreetingForFeet(rideDistance) {
-  // let rideDistance = 199;
-
-  if  rideDistance === 400 {
+  let message
+  if (rideDistance <= 400) {
     message = 'This one is on me!';
-  } else if {
-    rideDistance > 2000 {
-      message = 'I will gladly take your thirty bucks.';
-    } else {
-      rideDistance > 2500 {
-        message = 'No can do.';
-      }
-    }
   }
-
+  return message
 }
 
-function ternaryCheckCity() {
-
-}
-
-function switchOnCharmFromTip() {
-
-}
+// function scuberGreetingForFeet(rideDistance) {
+//   // let rideDistance = 199;
+// 
+//   if  rideDistance === 199 {
+//     message = 'This one is on me!';
+//   } else if {
+//     rideDistance > 2000 {
+//       message = 'I will gladly take your thirty bucks.';
+//     } else {
+//       rideDistance > 2500 {
+//         message = 'No can do.';
+//       }
+//     }
+//   }
+// 
+// }
+// 
+// function ternaryCheckCity() {
+// 
+// }
+// 
+// function switchOnCharmFromTip() {
+// 
+// }
