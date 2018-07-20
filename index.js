8eb5c687335d7e54d3046910351ccbf1046a429c
Yomaira Escano <yjescano@gmail.com>
2018-07-20 04:30:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,32 +6,16 @@ function scuberGreetingForFeet(rideDistance) {
     message = 'This one is on me!';
   } else if (rideDistance > 2000 && rideDistance < 2500) {
     message = 'I will gladly take your thirty bucks.';
-  } else if (rideDistance > 2500) {
+  } else (rideDistance > 2500) {
     message = 'No can do.';
   }
   return message
 }
 
-// function scuberGreetingForFeet(rideDistance) {
-//   // let rideDistance = 199;
-//
-//   if  rideDistance === 199 {
-//     message = 'This one is on me!';
-//   } else if {
-//     rideDistance > 2000 {
-//       message = 'I will gladly take your thirty bucks.';
-//     } else {
-//       rideDistance > 2500 {
-//         message = 'No can do.';
-//       }
-//     }
-//   }
-//
-// }
-//
-// function ternaryCheckCity() {
-//
-// }
+function ternaryCheckCity(city) {
+  let message;
+
+}
 //
 // function switchOnCharmFromTip() {
 //
