37a7815bd4ece6e5c0c9cd2eb4f154770974718c
Yomaira Escano <yjescano@gmail.com>
2018-07-20 02:57:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Write your code in this file!
-function scuberGreetingForFeet() {
-
+function scuberGreetingForFeet(someValue) {
+  if someValue <= 400;
+    'This one is on me!'
 }
 
 function ternaryCheckCity() {
-  
+
 }
 
 function switchOnCharmFromTip() {
-  
+
 }
